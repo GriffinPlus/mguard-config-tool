32bcb42e784dedc5f0519522721c7dcc192766c3
Sascha Falk <sascha@falk-online.eu>
2019-12-05 12:46:17 +0100

    Fix quoting issue when writing a simple value in an ATV document

--- mguard/atv/SimpleValue.go
+++ mguard/atv/SimpleValue.go
@@ -2,6 +2,7 @@ package atv
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -12,8 +13,7 @@ type SimpleValue struct {
 
 // WriteDocumentPart writes a part of the ATV document to the specified writer.
 func (value *SimpleValue) WriteDocumentPart(writer *strings.Builder, indent int) error {
-
-	line := fmt.Sprintf("\"%s\"\n", value.Value)
+	line := fmt.Sprintf("%s\n", strconv.Quote(value.Value))
 	_, err := writer.WriteString(line)
 	return err
 }
