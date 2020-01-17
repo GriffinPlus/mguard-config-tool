1448d115de750ed3cf9d0850dd56d3bb08b37d4e
Sascha Falk <sascha@falk-online.eu>
2020-01-17 11:59:51 +0100

    Let loadConfigurationFile() abort, if file does not exist

--- app/helpers.go
+++ app/helpers.go
@@ -54,60 +54,66 @@ func loadConfigurationFile(path string) (*ecs.Container, error) {
 		}
 
 		return nil, fmt.Errorf("Configuration file was not specified and stdin is no pipe")
-
-	} else {
-
-		log.Infof("Trying to load file (%s)...", path)
-
-		ext := strings.ToLower(filepath.Ext(path))
-		var tryOrder []string
-		if ext == ".atv" {
-			// this is probably an ATV file
-			log.Infof("File (%s) has the extension '%s'. This could be an ATV file.", path, ext)
-			tryOrder = []string{"atv", "ecs"}
-		} else if ext == ".tgz" {
-			// this could be an ECS container
-			log.Infof("File (%s) has the extension '%s'. This could be an ECS file.", path, ext)
-			tryOrder = []string{"ecs", "atv"}
-		} else {
-			// cannot give an educated guess
-			// => try both and check whether one works...
-			log.Infof("File (%s) has the extension '%s'. Cannot guess the configuration file type from the file extension.", path, ext)
-			tryOrder = []string{"ecs", "atv"}
-		}
-
-		for _, format := range tryOrder {
-			switch format {
-
-			case "atv":
-				log.Infof("Trying to interpret file (%s) as an ATV file...", path)
-				atv, err := atv.DocumentFromFile(path)
-				if err != nil {
-					log.Infof("Reading file (%s) failed: %s", path, err)
-					continue
-				}
-				log.Infof("Reading file (%s) succeeded.", path)
-				container := ecs.ContainerFromATV(atv)
-				return container, nil
-
-			case "ecs":
-				log.Infof("Trying to interpret file (%s) as an ECS file...", path)
-				container, err := ecs.ContainerFromFile(path)
-				if err != nil {
-					log.Infof("Reading file (%s) failed: %s", path, err)
-					continue
-				}
-				log.Infof("Reading file (%s) succeeded.", path)
-				return container, nil
-
-			default:
-				log.Panic("Unhandled document format")
-			}
-		}
-
-		// the file could neither be read as an ECS container nor as an ATV file
-		return nil, fmt.Errorf("Loading file (%s) failed", path)
 	}
+
+	log.Infof("Trying to load file (%s)...", path)
+
+	ext := strings.ToLower(filepath.Ext(path))
+	var tryOrder []string
+	if ext == ".atv" {
+		// this is probably an ATV file
+		log.Infof("File (%s) has the extension '%s'. This could be an ATV file.", path, ext)
+		tryOrder = []string{"atv", "ecs"}
+	} else if ext == ".tgz" {
+		// this could be an ECS container
+		log.Infof("File (%s) has the extension '%s'. This could be an ECS file.", path, ext)
+		tryOrder = []string{"ecs", "atv"}
+	} else {
+		// cannot give an educated guess
+		// => try both and check whether one works...
+		log.Infof("File (%s) has the extension '%s'. Cannot guess the configuration file type from the file extension.", path, ext)
+		tryOrder = []string{"ecs", "atv"}
+	}
+
+loop:
+	for _, format := range tryOrder {
+		switch format {
+
+		case "atv":
+			log.Infof("Trying to interpret file (%s) as an ATV file...", path)
+			atv, err := atv.DocumentFromFile(path)
+			if err != nil {
+				log.Infof("Reading file (%s) failed: %s", path, err)
+				if os.IsNotExist(err) {
+					break loop
+				}
+				continue
+			}
+			log.Infof("Reading file (%s) succeeded.", path)
+			container := ecs.ContainerFromATV(atv)
+			return container, nil
+
+		case "ecs":
+			log.Infof("Trying to interpret file (%s) as an ECS file...", path)
+			container, err := ecs.ContainerFromFile(path)
+			if err != nil {
+				log.Infof("Reading file (%s) failed: %s", path, err)
+				if os.IsNotExist(err) {
+					break loop
+				}
+				continue
+			}
+			log.Infof("Reading file (%s) succeeded.", path)
+			return container, nil
+
+		default:
+			log.Panic("Unhandled document format")
+		}
+	}
+
+	// the file could neither be read as an ECS container nor as an ATV file
+	return nil, fmt.Errorf("Loading file (%s) failed", path)
+
 }
 
 // exePath gets the full path of the executable.
