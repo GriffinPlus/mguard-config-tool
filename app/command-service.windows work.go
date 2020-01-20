f4390ea766c9d748a4a073364640fb67f14adece
Sascha Falk <sascha@falk-online.eu>
2020-01-20 15:15:40 +0100

    Add watching input directory, trigger processing after stabilizing

--- app/command-service.windows work.go
+++ app/command-service.windows work.go
@@ -2,4 +2,8 @@
 
 package main
 
-
+// processFileInHotfolder is called when a new file arrives in the input directory for configuration files.
+// It processes .atv/.ecs files, merges them with the common configuration and writes them to the output directory.
+func (cmd *ServiceCommand) processFileInHotfolder(path string) error {
+	return nil
+}
--- app/command-service.windows_svcctrl.go
+++ app/command-service.windows_svcctrl.go
@@ -4,8 +4,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
+	"github.com/fsnotify/fsnotify"
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/sys/windows/svc"
 	"golang.org/x/sys/windows/svc/eventlog"
 	"golang.org/x/sys/windows/svc/mgr"
@@ -13,12 +17,63 @@ import (
 
 // Execute is the handler for commands coming in from the service control manager.
 func (cmd *ServiceCommand) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (ssec bool, errno uint32) {
-	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown
+
+	// signal that the service is starting
 	changes <- svc.Status{State: svc.StartPending}
-	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+
+	// initialize file system watcher for input directory (hot folder)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Errorf("%v", err)
+		changes <- svc.Status{State: svc.StopPending}
+		return
+	}
+	defer watcher.Close()
+
+	// start watching hot folder
+	err = watcher.Add(cmd.watchedConfigurationDirectory)
+	if err != nil {
+		log.Errorf("%v", err)
+		changes <- svc.Status{State: svc.StopPending}
+		return
+	}
+
+	// signal that the service is running now
+	changes <- svc.Status{
+		State:   svc.Running,
+		Accepts: svc.AcceptStop | svc.AcceptShutdown,
+	}
+
+	// populate list of files in hot folder to start with
+	filesInHotFolder := make(map[string]time.Time)
+	err = filepath.Walk(cmd.watchedConfigurationDirectory, func(path string, info os.FileInfo, err error) error {
+		path, err = filepath.Abs(path)
+		if err == nil {
+			isConfFile, err := isPossiblemGuardConfigurationFile(path)
+			if err == nil && isConfFile {
+				log.Debugf("Found possible configuration file: %s", path)
+				filesInHotFolder[path] = time.Now()
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Errorf("%v", err)
+		changes <- svc.Status{State: svc.StopPending}
+		return
+	}
+
+	// set up timer that triggers processing files in the hot folder
+	hotFolderDetectedFileCooldown := 5 * time.Second          // time after which a file is considered "stable"
+	hotFolderProcessingCycle := 1 * time.Second               // time after which the files in the hot folder are checked
+	hotFolderTimer := time.NewTimer(hotFolderProcessingCycle) // timer triggering processing files in the hot folder
+	hotFolderTimerRunning := true
+
 loop:
 	for {
 		select {
+
+		// handle service control commands
 		case c := <-r:
 			switch c.Cmd {
 			case svc.Interrogate:
@@ -31,8 +86,104 @@ loop:
 			default:
 				cmd.elog.Error(1, fmt.Sprintf("Unexpected control request #%d", c))
 			}
+
+		// handle file system watcher events
+		case event, ok := <-watcher.Events:
+
+			if !ok {
+				continue
+			}
+
+			log.Debugf("fs event: %v", event)
+
+			if (event.Op & fsnotify.Create) == fsnotify.Create {
+				log.Debugf("Created file: %s", event.Name)
+				path, err := filepath.Abs(event.Name)
+				if err == nil {
+					isConfFile, err := isPossiblemGuardConfigurationFile(path)
+					if err == nil && isConfFile {
+						filesInHotFolder[path] = time.Now()
+						if !hotFolderTimerRunning {
+							hotFolderTimer.Reset(hotFolderProcessingCycle)
+							hotFolderTimerRunning = true
+						}
+					}
+				}
+			}
+
+			if event.Op&(fsnotify.Remove|fsnotify.Rename) != 0 {
+
+				log.Debugf("Removed/Renamed file: %s", event.Name)
+				path, err := filepath.Abs(event.Name)
+				if err == nil {
+					delete(filesInHotFolder, path)
+				}
+				if len(filesInHotFolder) == 0 {
+					if hotFolderTimerRunning {
+						if !hotFolderTimer.Stop() {
+							<-hotFolderTimer.C
+						}
+						hotFolderTimerRunning = false
+					}
+				}
+			}
+
+			if (event.Op & fsnotify.Write) == fsnotify.Write {
+				log.Debugf("Modified file: %s", event.Name)
+				path, err := filepath.Abs(event.Name)
+				if err == nil {
+					isConfFile, err := isPossiblemGuardConfigurationFile(path)
+					if err == nil && isConfFile {
+						filesInHotFolder[path] = time.Now()
+						if !hotFolderTimerRunning {
+							hotFolderTimer.Reset(hotFolderProcessingCycle)
+							hotFolderTimerRunning = true
+						}
+					}
+				}
+			}
+
+		// handle file system watcher errors
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				continue
+			}
+			log.Errorf("watcher error: %v", err)
+
+		// process files in the hot folder that did not change within the specified time
+		case <-hotFolderTimer.C:
+			for file, lastwritten := range filesInHotFolder {
+				if time.Since(lastwritten) > hotFolderDetectedFileCooldown {
+					path, _ := filepath.Abs(file)
+					err = cmd.processFileInHotfolder(path)
+					if err == nil {
+						log.Infof("Processing file '%s' succeeded.", path)
+						err = os.Remove(path)
+						if err != nil {
+							log.Errorf("%v", err)
+						} else {
+							log.Debugf("Removing file '%s' succeeded.", path)
+						}
+					} else {
+						newPath := path + ".err"
+						log.Errorf("Processing file '%s' failed, renaming it to '%s'.", path, newPath)
+						err = os.Rename(path, newPath)
+						if err != nil {
+							log.Errorf("Renaming file '%s' to '%s' failed: %v", path, newPath, err)
+						}
+					}
+					delete(filesInHotFolder, path)
+				}
+			}
+			if len(filesInHotFolder) > 0 {
+				hotFolderTimer.Reset(hotFolderProcessingCycle)
+				hotFolderTimerRunning = true
+			} else {
+				hotFolderTimerRunning = false
+			}
 		}
 	}
+
 	changes <- svc.Status{State: svc.StopPending}
 	return
 }
