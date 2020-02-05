3e591ed2f65cbf7e97c1640cddb2fed58d1d031b
Sascha Falk <sascha@falk-online.eu>
2020-02-05 10:06:37 +0100

    Add Compare() function to atv.Version

--- mguard/atv/Version.go
+++ mguard/atv/Version.go
@@ -12,6 +12,40 @@ type Version struct {
 	Suffix string
 }
 
+// Compare compares the current version with the specified one.
+// Returns -1, if the current version is less than the specified one.
+// Returns 0, if the current version equals the current one.
+// Returns +1, if the current version is greater than the specified one.
+func (version Version) Compare(other Version) int {
+
+	// check major version
+	if version.Major < other.Major {
+		return -1
+	}
+	if version.Major > other.Major {
+		return 1
+	}
+
+	// same major version, check minor version
+	if version.Minor < other.Minor {
+		return -1
+	}
+	if version.Minor > other.Minor {
+		return 1
+	}
+
+	// same minor version, check patch version
+	if version.Patch < other.Patch {
+		return -1
+	}
+	if version.Patch > other.Patch {
+		return 1
+	}
+
+	// the versions are equal
+	return 0
+}
+
 // String returns the version as a string.
 func (version Version) String() string {
 	s := fmt.Sprintf("%d.%d.%d", version.Major, version.Minor, version.Patch)
